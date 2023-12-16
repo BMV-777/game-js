5d514fcd00dafd4f84d3d643bd639b1dd3ff44f3
Maks Borenkov <borenkovmv@gmail.com>
2023-12-16 21:49:49 +0200

    block1

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
 
-document.querySelector('.guess-message').textContent = 'Правильно!';
+const refs = {
+  btn: document.querySelector('.check'),
+  input: document.querySelector('.number-input'),
+  message: document.querySelector('.guess-message'),
+  question: document.querySelector('.question'),
+};
 
-76;
+refs.btn.addEventListener('click', addClick);
+function addClick() {
+  let event = Number(refs.input.value);
+  let messages = (refs.message.textContent = 'Правильно!');
+  let quest = (refs.question.textContent = event);
+
+  console.log(event, typeof event);
+  console.log(typeof messages);
+  console.log(typeof quest);
+  if (!event) {
+    refs.message.textContent = 'Ведите число!!!';
+  } else {
+    messages;
+  }
+}
+77/
